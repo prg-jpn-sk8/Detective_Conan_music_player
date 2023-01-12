7d75be9b9d0293ac449cc212c6affba91c164342
prg-jpn-sk8 <121560023+prg-jpn-sk8@users.noreply.github.com>
2023-01-12 16:19:46 +0300

    Update App.py

--- src/App.py
+++ src/App.py
@@ -6,14 +6,18 @@ screen.title('Detective Conan music')
 screen.iconphoto(False, tk.PhotoImage('icons/icon.png'))
 
 def play():
-    ps.playsound('music/DetectiveConan.mp3', False) 
+    ps.playsound('music/DetectiveConan.mp3', False)
+def plays():
+    ps.playsound('music/DetectiveConanSad.mp3', False)
+def playss():
+    ps.playsound('music/naruto-war-music.mp3', False) 
 def Exit():
-    exit()
+    tk.exit()
 screen.geometry('900x400')
 
-
-
-Text = tk.Label(screen, text="Play Detective Conan music").pack(pady=40)
-Play = tk.Button(screen, text="Play Detective conan", command=play).pack(pady =20)
-Exit = tk.Button(screen, text="Exit", command=Exit).pack(pady=50)
+Text = tk.Label(screen, text="Play Detective Conan music").pack(pady=20)
+Play = tk.Button(screen, text="Play Detective conan music", command=play).pack(pady =20)
+Plays = tk.Button(screen, text="Play Detective conan sad music", command=plays).pack(pady =20)
+Playss = tk.Button(screen, text="Play Naruto music", command=playss).pack(pady =20)
+Exit = tk.Button(screen, text="Exit", command=Exit).pack(pady=5)   
 screen.mainloop()
