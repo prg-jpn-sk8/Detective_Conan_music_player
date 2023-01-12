5c8959944033477c367daaadbd178261c34a8923
prg-jpn-sk8 <121560023+prg-jpn-sk8@users.noreply.github.com>
2023-01-12 18:07:02 +0300

    Update App.py

--- src/App.py
+++ src/App.py
@@ -1,23 +1,31 @@
 import tkinter as tk
 import playsound as ps
+from random import *
 
 screen = tk.Tk()
 screen.title('Detective Conan music')
 screen.iconphoto(False, tk.PhotoImage('icons/icon.png'))
 
+
 def play():
-    ps.playsound('music/DetectiveConan.mp3', False)
+        ps.playsound('music/DetectiveConan.mp3', False)
 def plays():
     ps.playsound('music/DetectiveConanSad.mp3', False)
 def playss():
     ps.playsound('music/naruto-war-music.mp3', False) 
 def Exit():
-    tk.exit()
+    exit()
 screen.geometry('900x400')
-
-Text = tk.Label(screen, text="Play Detective Conan music").pack(pady=20)
-Play = tk.Button(screen, text="Play Detective conan music", command=play).pack(pady =20)
-Plays = tk.Button(screen, text="Play Detective conan sad music", command=plays).pack(pady =20)
-Playss = tk.Button(screen, text="Play Naruto music", command=playss).pack(pady =20)
+def playsss():
+    while True:
+        while True:
+            list = ["music/DetectiveConan.mp3", "music/DetectiveConanSad.mp3", "music/naruto-war-music.mp3"]
+            music = choice(list)
+            ps.playsound(music , False)
+Text = tk.Label(screen, text="Play Detective Conan music").pack(pady=10)
+Play = tk.Button(screen, text="Play Detective conan music", command=play).pack(pady =15)
+Plays = tk.Button(screen, text="Play Detective conan sad music", command=plays).pack(pady =15)
+Playss = tk.Button(screen, text="Play Naruto music", command=playss).pack(pady = 15)
+Playsss= tk.Button(screen, text="random-loop", command=playsss).pack(pady =15)
 Exit = tk.Button(screen, text="Exit", command=Exit).pack(pady=5)   
 screen.mainloop()
